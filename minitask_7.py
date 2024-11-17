264e8cab1f776f9457f64ea4f93cb2f2fea0ba7a
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2024-11-17 06:45:28 +0000

    Reviewed

--- minitask_7.py
+++ minitask_7.py
@@ -1,18 +1,35 @@
+import functools
+
 def deprecated(since = None, will_be_removed = None):
-    def inner(f):
+    if callable(since):
+        func = since
+
+        @functools.wraps(func)
         def wrapper(*args, **kwargs):
-            if since and will_be_removed:
-                warn_mssg = f"Warning: function {f.__name__} is deprecated since version {since}. It will be removed in version {will_be_removed}."
-            elif will_be_removed:
-                warn_mssg = f"Warning: function {f.__name__} is deprecated. It will be removed in version {will_be_removed}."
-            elif since:
-                warn_mssg = f"Warning: function {f.__name__} is deprecated since version {since}. It will be removed in future versions."
-            else:
-                warn_mssg = f"Warning: function {f.__name__} is deprecated. It will be removed in future versions."
+            print(f"Warning: function {func.__name__} is deprecated. It will be removed in future versions.")
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    def inner(f):
+        @functools.wraps(f)
+        def wrapper(*args, **kwargs):
+            prefix = f"Warning: function {f.__name__} is deprecated"
+            main_message = f"."
+            postfix = f" It will be removed in future versions."
+            if since:
+                main_message = f" since version {since}."
+            if will_be_removed:
+                postfix = f" It will be removed in version {will_be_removed}."
+            warn_mssg = prefix + main_message + postfix
             print(warn_mssg)
             return f(*args, **kwargs), warn_mssg
         return wrapper
     return inner
+
+@deprecated(since= "4.2.0", will_be_removed= "5.0.1")
+def sum(x, y):
+    return x+y
 
 @deprecated(since= "4.2.0", will_be_removed= "5.0.1")
 def foo():
@@ -26,7 +43,9 @@ def bar():
 def baz():
     print("Hello from baz")
 
-
+result, warning = sum(4, 6)
+assert warning == "Warning: function sum is deprecated since version 4.2.0. It will be removed in version 5.0.1."
+print(result)
 result, warning = foo()
 assert warning == "Warning: function foo is deprecated since version 4.2.0. It will be removed in version 5.0.1."
 result, warning = bar()
