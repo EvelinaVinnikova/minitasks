9b5bab0f0ce11d57606e74f54f549fffac74887c
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2024-12-05 12:44:33 +0000

    Reviewed

--- 3_minitask.py
+++ 3_minitask.py
@@ -2,12 +2,24 @@ def parse_matrix(input_string):
     rows = input_string.split('|')
     matrix = []
     for row in rows:
-        float_row = [float(num) for num in row.strip().split() if num]
+        float_row = [float(num) for num in row.strip().split()]
         matrix.append(float_row)
-
     return matrix
 
 input_data = "1 2 | 3 4"
 matrix = parse_matrix(input_data)
 print(matrix)
-print(matrix[0][1])
+assert matrix == [[1.0, 2.0], [3.0, 4.0]]
+input_data1 = "12  4| -1 5  | 3 5 7 | 7 10 11111  "
+matrix = parse_matrix(input_data1)
+print(matrix)
+assert matrix == [[12.0, 4.0], [-1.0, 5.0], [3.0, 5.0, 7.0], [7.0, 10.0, 11111.0]]
+input_data2 = "10000  6| -1000 7.9"
+matrix = parse_matrix(input_data2)
+print(matrix)
+assert matrix == [[10000.0, 6.0], [-1000.0, 7.9]]
+input_data3 = "                  10.9999  4| -1 5 | 8686 | 6 5  4 7 433 57 "
+matrix = parse_matrix(input_data3)
+print(matrix)
+assert matrix == [[10.9999, 4.0], [-1.0, 5.0], [8686.0], [6.0, 5.0, 4.0, 7.0, 433.0, 57.0]]
+
