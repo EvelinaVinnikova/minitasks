24269c23c80a4194646b51821681c611fb7d8fd9
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2024-12-05 15:52:15 +0000

    Rev

--- 4_minitask.py
+++ 4_minitask.py
@@ -1,24 +1,24 @@
 def create_reverse_dict(d):
     reverse_dict = {}
     for key, value in d.items():
-        if not isinstance(value, (int, float, str, bool, tuple)):
-            raise TypeError(
-                f"Значение {value} не может быть использовано в качестве ключа, так как оно не является хешируемым.")
-
         if value in reverse_dict:
-            existing_value = reverse_dict[value]
-            if isinstance(existing_value, tuple):
-                reverse_dict[value] = existing_value + (key,)
-            else:
-                reverse_dict[value] = (existing_value, key)
+            exist_value = reverse_dict[value] if isinstance(reverse_dict[value], tuple) else (reverse_dict[value],)
+            reverse_dict[value] = exist_value + (key,)
         else:
-            reverse_dict[value] = key
-
+            reverse_dict[value] = (key,) if isinstance(key, tuple) else key
     return reverse_dict
 
-d = {"Ivanov": 97832, "Petrov": 55521, "Kuznecov": 97832}
-try:
-    reversed_d = create_reverse_dict(d)
-    print(reversed_d)
-except TypeError as e:
-    print(e)
+d = {("Petrov",): 123, "Kuznezov": 123}
+reversed_d = create_reverse_dict(d)
+assert reversed_d == {123: (('Petrov',), 'Kuznezov')}
+print(reversed_d)
+
+input_dict = {"Ivanov": 97832, "Petrov": 55521, ("Kuznecov",): 97832}
+reversed_d = create_reverse_dict(input_dict)
+assert reversed_d == {97832: ('Ivanov', ('Kuznecov',)), 55521: 'Petrov'}
+print(reversed_d)
+
+input_dict = {"Alice": 42, "Bob": 42, "Charlie": 123}
+reversed_d = create_reverse_dict(input_dict)
+assert reversed_d == {42: ('Alice', 'Bob'), 123: 'Charlie'}
+print(reversed_d)
