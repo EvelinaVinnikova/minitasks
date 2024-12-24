74f453dc273c592b07572f1bd972e0370d392433
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2024-12-24 12:47:49 +0000

    Reviewed_2

--- minitask_7.py
+++ minitask_7.py
@@ -1,62 +1,51 @@
 import functools
 
-def deprecated(since = None, will_be_removed = None):
-    if callable(since):
-        func = since
+def deprecated(func=None, *, since=None, will_be_removed=None):
+    if func is None:
+        return lambda f: deprecated(f, since=since, will_be_removed=will_be_removed)
 
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            warn_mssg = f"Warning: function {func.__name__} is deprecated. It will be removed in future versions."
-            print(warn_mssg)
-            return func(*args, **kwargs), warn_mssg
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        prefix = f"Warning: function {func.__name__} is deprecated"
+        main_message = f"."
+        postfix = f" It will be removed in future versions."
+        if since:
+            main_message = f" since version {since}."
+        if will_be_removed:
+            postfix = f" It will be removed in version {will_be_removed}."
+        warn_msg = prefix + main_message + postfix
+        print(warn_msg)
+        return func(*args, **kwargs)
 
-        return wrapper
+    return wrapper
 
-    def inner(f):
-        @functools.wraps(f)
-        def wrapper(*args, **kwargs):
-            prefix = f"Warning: function {f.__name__} is deprecated"
-            main_message = f"."
-            postfix = f" It will be removed in future versions."
-            if since:
-                main_message = f" since version {since}."
-            if will_be_removed:
-                postfix = f" It will be removed in version {will_be_removed}."
-            warn_mssg = prefix + main_message + postfix
-            print(warn_mssg)
-            return f(*args, **kwargs), warn_mssg
-        return wrapper
-    return inner
-
-@deprecated(since= "4.2.0", will_be_removed= "5.0.1")
+@deprecated(since="4.2.0", will_be_removed="5.0.1")
 def sum(x, y):
-    return x+y
+    return x + y
 
 @deprecated
 def subst(x, y):
-    return x-y
-print(subst(7,1))
-@deprecated(since= "4.2.0", will_be_removed= "5.0.1")
+    return x - y
+
+@deprecated(since="4.2.0", will_be_removed="5.0.1")
 def foo():
     print("Hello from foo")
 
-@deprecated(since= "of the Earth with dinosaurs")
+@deprecated(since="of the Earth with dinosaurs")
 def bar():
     print("Hello from bar")
 
-@deprecated(will_be_removed= "when pig fly")
+@deprecated(will_be_removed="when pigs fly")
 def baz():
     print("Hello from baz")
 
-result, warning = sum(4, 6)
-assert warning == "Warning: function sum is deprecated since version 4.2.0. It will be removed in version 5.0.1."
+# Testing the functions
+result = sum(4, 6)
 print(result)
-result, warning = subst(7, 1)
-assert warning == "Warning: function subst is deprecated. It will be removed in future versions."
+
+result = subst(7, 1)
 print(result)
-result, warning = foo()
-assert warning == "Warning: function foo is deprecated since version 4.2.0. It will be removed in version 5.0.1."
-result, warning = bar()
-assert warning == "Warning: function bar is deprecated since version of the Earth with dinosaurs. It will be removed in future versions."
-result, warning = baz()
-assert warning == "Warning: function baz is deprecated. It will be removed in version when pig fly."
+
+foo()
+bar()
+baz()
