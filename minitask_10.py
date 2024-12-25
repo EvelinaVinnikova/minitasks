a6a7b38e778329d9482273501b432022f2dafcf6
Evanderer <125723482+EvelinaVinnikova@users.noreply.github.com>
2024-12-25 17:07:20 +0000

    Reviewed

--- minitask_10.py
+++ minitask_10.py
@@ -1,12 +1,18 @@
 def singleton(cls):
-    instances = {}
+    instance = None
+    class Wrapper:
+        def __init__(self, *args, **kwargs):
+            self._cls = cls
 
-    def get_instance(*args, **kwargs):
-        if cls not in instances:
-            instances[cls] = cls(*args, **kwargs)
-        return instances[cls]
+        def __call__(self, *args, **kwargs):
+            nonlocal instance
+            if instance is None:
+                instance = self._cls(*args, **kwargs)
+            return instance
 
-    return get_instance
+        def __getattr__(self, name):
+            return getattr(self._cls, name)
+    return Wrapper()
 
 @singleton
 class GlobalCounter:
@@ -17,11 +23,18 @@ class GlobalCounter:
     def increment(self):
         self.count += self.step
 
+@singleton
+class Test:
+    some_global_field = 123
 
+
+# Testing
+print(Test.some_global_field)
 gc1 = GlobalCounter(initial_count=10, step=2)
 gc2 = GlobalCounter()
 
 assert id(gc1) == id(gc2)
+
 print(gc1.count)
 gc1.increment()
 print(gc2.count)
